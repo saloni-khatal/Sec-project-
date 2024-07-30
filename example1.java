bc34430e5700f17bd7feffb08136546b05bb1933
saloni-khatal <153343513+saloni-khatal@users.noreply.github.com>
2024-07-30 22:47:21 +0530

    Update Example1.java

--- example1.java
+++ example1.java
@@ -1,6 +1,6 @@
 public class Example1
 {
-     public static void main(String args []) 
+     public static void main(String[] args) 
      {
         Example1 example = new Example1();
         example.displayMessage();
