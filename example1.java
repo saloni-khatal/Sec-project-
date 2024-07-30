c9cc5fd532617e16813f83fd6821a7422552afa8
saloni-khatal <153343513+saloni-khatal@users.noreply.github.com>
2024-07-30 22:46:33 +0530

    Update example1.java

--- example1.java
+++ example1.java
@@ -1,8 +1,14 @@
-class example1
+public class Example1
 {
-    
-       public static void main(String args []) 
-{
-       System.out.println("This is a simple Java program.");
-       }
+     public static void main(String args []) 
+     {
+        Example1 example = new Example1();
+        example.displayMessage();
+     }
+     public void displayMessage()
+     {
+         System.out.println("this is simple java program");
+     }
 }
+
+       
